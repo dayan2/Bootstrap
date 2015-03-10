c503e92846f4faa39eafa2bd17491f705577545f
dayan2 <dayanmendis2@gmail.com>
2015-03-10 21:46:41 +0530

    s

---
+++ 13 - Jumbotron-Example/Jumbotron-Example-1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jumbotron_Example_1.Startup))]
+namespace Jumbotron_Example_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
